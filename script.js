b469bcd5fb86a537fb5e39b3a6537af8dc8ea8ff
Anujku007 <anujkumar6205158@gmail.com>
2025-01-06 12:31:07 +0530

    Add files via upload
    First Upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let searchForm = document.querySelector('.search-form')
+
+document.querySelector('#search-btn').onclick = () =>
+{
+	searchForm.classList.toggle('active');
+}
+
+
+let shopingCart = document.querySelector('.shoping-cart')
+
+document.querySelector('#cart-btn').onclick = () =>
+{
+	shopingCart.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form')
+
+document.querySelector('#login-btn').onclick = () =>
+{
+	loginForm.classList.toggle('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
